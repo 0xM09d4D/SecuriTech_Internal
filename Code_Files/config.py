e7b59566b8537c93835109c7ea44f8a49d4389bb
0xM09d4D <mogdadshampoin@hotmail.com>
2025-02-11 23:35:13 +0100

    Fixed security issue, removed sensitive info

--- Code_Files/config.py
+++
@@ -1,6 +0,0 @@
-# Internal configuration file
-DB_PASSWORD = ""  # Change before deployment
-API_KEY = "test-key-xyz"
-
-# TODO: Remove before production!
-# Flag: SecurinetsEPS{Mf_F0rGo7_7o_D3L3Te}
